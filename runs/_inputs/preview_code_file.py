c9f00ff367bde9edbefd0689324ba54675160da4
tafreeman <72930272+tafreeman@users.noreply.github.com>
2026-02-16 04:32:03 -0600

    trim

--- runs/_inputs/preview_code_file.py
+++ runs/_inputs/preview_code_file.py
@@ -1,16 +1,12 @@
-class Pair(object): 
-	def __init__(self, a, b): 
-		self.a = a 
-		self.b = b 
-def max_chain_length(arr, n): 
-	max = 0
-	mcl = [1 for i in range(n)] 
-	for i in range(1, n): 
-		for j in range(0, i): 
-			if (arr[i].a > arr[j].b and
-				mcl[i] < mcl[j] + 1): 
-				mcl[i] = mcl[j] + 1
-	for i in range(n): 
-		if (max < mcl[i]): 
-			max = mcl[i] 
-	return max
+diff --git a/astropy/modeling/separable.py b/astropy/modeling/separable.py
+--- a/astropy/modeling/separable.py
++++ b/astropy/modeling/separable.py
+@@ -242,7 +242,7 @@ def _cstack(left, right):
+         cright = _coord_matrix(right, 'right', noutp)
+     else:
+         cright = np.zeros((noutp, right.shape[1]))
+-        cright[-right.shape[0]:, -right.shape[1]:] = 1
++        cright[-right.shape[0]:, -right.shape[1]:] = right
+ 
+     return np.hstack([cleft, cright])
+ 
